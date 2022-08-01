0a27a788b61374ccc5c7aa33c88aa2f25ae6607e
Alao-Abiodun <46402042+Alao-Abiodun@users.noreply.github.com>
2022-08-01 08:55:44 +0100

    ch:add test for endpoint to create new users

--- __test__/route.test.js
+++ __test__/route.test.js
@@ -7,5 +7,22 @@ describe('User API', () => {
         expect(res.status).toEqual(200);
         expect(res.body).toHaveProperty('users');
     });
+
+    it('should show a user', async () => {
+        const res = await request(app).get('/api/v1/users/1')
+        expect(res.status).toEqual(200);
+        expect(res.body).toHaveProperty('user');
+    })
+
+    it('should create a user', async () => {
+        const res = await request(app).post('/api/v1/users').send({
+            firstName: 'John1',
+            lastName: 'Doe1',
+            email: 'johndoe1@gmail.com',
+            password: 'password1',
+        });
+        expect(res.statusCode).toEqual(201);
+        expect(res.body).toHaveProperty('user');
+    })
 })
 
--- server.js
+++ server.js
@@ -13,12 +13,14 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Welcome To The User Project API'
-    })
-})
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Welcome To The User Project API'
+//     })
+// })
 
 app.use('/api/v1', routes);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+
+module.exports = app;
